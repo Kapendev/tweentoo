41f7a2bd55f19ea70517ed952334e7a021b5f512
AlexandrosKap <53071962+AlexandrosKap@users.noreply.github.com>
2021-06-24 23:18:08 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Tweentoo
 An easy to use tween script for moving, rotating and scaling Node2D or Spatial nodes for the Godot engine.<br/>
-You can watch a video about it here: https://youtu.be/79pTFbRVaZI
+The modulate and self_modulate property of Node2D nodes can also change.<br/>
+You can watch a video about Tweentoo here: https://youtu.be/79pTFbRVaZI
 
 ![Tweentoo Image](https://img.itch.zone/aW1hZ2UvMTEwMDExOC82MzQ0OTYwLnBuZw==/original/U4SfjI.png)
 
@@ -8,3 +9,4 @@ How to use:
 1) Add a Tweentoo to a Node2D/Spatial.
 2) Add Node2D/Spatial nodes to the Tweentoo.
 3) Play the scene or call change().
+
--- src/Tweentoo.gd
+++ src/Tweentoo.gd
@@ -8,6 +8,7 @@ extends Tween
 # 1) Add a Tweentoo to a Node2D/Spatial.
 # 2) Add Node2D/Spatial nodes to the Tweentoo.
 # 3) Play the scene or call change().
+# Extra: You can also change the modulate and self_modulate property of Node2D nodes.
 
 signal tweentoo_completed
 signal tweentoo_started
@@ -15,6 +16,7 @@ signal tweentoo_started
 enum TransitionType {LINEAR = 0, CUBIC = 7, CIRC = 8, ELASTIC = 6, BOUNCE = 9, BACK = 10}
 enum EaseType {IN_OUT = 2, OUT_IN = 3, IN = 0, OUT = 1}
 
+export var reversed_change := false setget set_reversed_change
 export var can_start := true
 export var can_loop := true
 export var update_position := true
@@ -26,15 +28,15 @@ export(EaseType) var easing := EaseType.IN_OUT
 export(float, 0, 172800) var duration := 1.0
 export(Array, float) var duration_array := []
 
-onready var current_child := get_child_count()
+var current_child := -1
 var current_duration := 0
 var is_2D := true
-var is_changing = false
+var is_changing := false
 
 func _ready() -> void:
 	# warning-ignore:return_value_discarded
 	connect("tween_all_completed", self, "_on_Tweentoo_tween_all_completed")
-	var parent = get_parent()
+	var parent := get_parent()
 	if !parent is Node2D:
 		is_2D = false
 	if !is_2D and !parent is Spatial:
@@ -45,71 +47,74 @@ func _ready() -> void:
 
 func next_child() -> int:
 	var result := current_child
-	result += 1
+	if !reversed_change:
+		result += 1
+	else:
+		result -= 1
 	if result >= get_child_count():
-		result = 0
+		return 0
+	elif result < 0:
+		return get_child_count() - 1
 	return result
 
 func next_duration() -> int:
 	var result := current_duration
-	result += 1
+	if !reversed_change:
+		result += 1
+	else:
+		result -= 1
 	if result >= duration_array.size():
-		result = 0
+		return 0
+	elif result < 0:
+		return duration_array.size() - 1
 	return result
 
 func change() -> void:
 	is_changing = true
 	if get_child_count() != 0:
-		var pnode := get_parent()
-		var cnode := get_child(next_child())
-		if is_2D and cnode is Node2D:
-			if duration_array.empty():
-				if update_position:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "position", pnode.position, cnode.position, duration, transition, easing)
-				if update_rotation:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "rotation", pnode.rotation, cnode.rotation, duration, transition, easing)
-				if update_scale:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration, transition, easing)
-			else:
-				if update_position:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "position", pnode.position, cnode.position, duration_array[current_duration], transition, easing)
-				if update_rotation:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "rotation", pnode.rotation, cnode.rotation, duration_array[current_duration], transition, easing)
-				if update_scale:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration_array[current_duration], transition, easing)
-		elif !is_2D and cnode is Spatial:
-			if duration_array.empty():
-				if update_position:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "translation", pnode.translation, cnode.translation, duration, transition, easing)
-				if update_rotation:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "rotation_degrees", pnode.rotation_degrees, cnode.rotation_degrees, duration, transition, easing)
-				if update_scale:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration, transition, easing)
-			else:
-				if update_position:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "translation", pnode.translation, cnode.translation, duration_array[current_duration], transition, easing)
-				if update_rotation:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "rotation_degrees", pnode.rotation_degrees, cnode.rotation_degrees, duration_array[current_duration], transition, easing)
-				if update_scale:
-					# warning-ignore:return_value_discarded
-					interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration_array[current_duration], transition, easing)
+		if is_2D:
+			var pnode : Node2D = get_parent()
+			var cnode : Node2D = get_child(next_child())
+			if update_position and pnode.position != cnode.position:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "position", pnode.position, cnode.position, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if update_rotation and pnode.rotation != cnode.rotation:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "rotation", pnode.rotation, cnode.rotation, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if update_scale and pnode.scale != cnode.scale:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if pnode.modulate != cnode.modulate:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "modulate", pnode.modulate, cnode.modulate, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if pnode.self_modulate != cnode.self_modulate:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "self_modulate", pnode.self_modulate, cnode.self_modulate, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+		else:
+			var pnode : Spatial = get_parent()
+			var cnode : Spatial = get_child(next_child())
+			if update_position and pnode.translation != cnode.translation:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "translation", pnode.translation, cnode.translation, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if update_rotation and pnode.rotation_degrees != cnode.rotation_degrees:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "rotation_degrees", pnode.rotation_degrees, cnode.rotation_degrees, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
+			if update_scale and pnode.scale != cnode.scale:
+				# warning-ignore:return_value_discarded
+				interpolate_property(pnode, "scale", pnode.scale, cnode.scale, duration if duration_array.empty() else duration_array[current_duration], transition, easing)
 		# warning-ignore:return_value_discarded
 		start()
 
 func start_state() -> void:
-	current_child = get_child_count()
+	current_child = -1
 	current_duration = 0
+
+func set_reversed_change(value : bool) -> void:
+	reversed_change = value
+	# warning-ignore:return_value_discarded
+	remove_all()
+	if is_changing:
+		change()
 
 func _on_Tweentoo_tween_all_completed() -> void:
 	is_changing = false
@@ -118,10 +123,20 @@ func _on_Tweentoo_tween_all_completed() -> void:
 		current_duration = next_duration()
 	
 	if current_child == get_child_count() - 1:
-		emit_signal("tweentoo_completed")
+		if !reversed_change:
+			emit_signal("tweentoo_completed")
+		else:
+			emit_signal("tweentoo_started")
 	elif current_child == 0:
-		emit_signal("tweentoo_started")
+		if !reversed_change:
+			emit_signal("tweentoo_started")
+		else:
+			emit_signal("tweentoo_completed")
+	
 	if can_loop:
 		change()
-	elif !current_child == get_child_count() - 1:
-		change()
+	else:
+		if !reversed_change and current_child != get_child_count() - 1:
+			change()
+		elif reversed_change and current_child != 0:
+			change()
